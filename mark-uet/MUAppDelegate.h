9cb18425bb5bc699c1a2563d09bf170a8c482157
Ethan N <thanhnx.605@gmail.com>
2013-05-02 20:33:48 +0700

    Init commit

---
+++ mark-uet/MUAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MUAppDelegate.h
+//  mark-uet
+//
+//  Created by Ethan Nguyen on 5/2/13.
+//  Copyright (c) 2013 UET. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MUAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ mark-uet/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  mark-uet
+//
+//  Created by Ethan Nguyen on 5/2/13.
+//  Copyright (c) 2013 UET. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MUAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MUAppDelegate class]));
+    }
+}
